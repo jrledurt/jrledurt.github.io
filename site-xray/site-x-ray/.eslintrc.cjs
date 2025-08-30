28159d240ed2bdf084f7be469ebb5cff205b6c11
jrledurt <jrledurt@gmail.com>
2025-08-30 17:39:30 +0000

    Add Site X-Ray app and calculators

---
+++ site-xray/site-x-ray/.eslintrc.cjs
@@ -0,0 +1,14 @@
+module.exports = {
+  extends: [
+    'next',
+    'next/core-web-vitals',
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+  ],
+  plugins: ['@typescript-eslint'],
+  parser: '@typescript-eslint/parser',
+  rules: {
+    '@typescript-eslint/no-explicit-any': 'off',
+  },
+};
